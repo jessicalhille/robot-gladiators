118dffc2955bee6f19602b21579a93ad41084f96
Jessica Hille <jessicalhille@gmail.com>
2021-10-25 22:10:20 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,9 +7,6 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-// Alert players that they are starting the round
-window.alert("Welcome to Robot Gladiators!");
-
 // fight function
 var fight = function(enemyName) {
     // repeat and execute as long as the enemy-robot is alive
@@ -78,7 +75,18 @@ var fight = function(enemyName) {
   };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        //let player know what round they are in
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        // pick new enemy to fight based on the index of the enemyNames
+        var pickedEnemyName = enemyNames[i];
+        //reset enemyHealth before starting new fight
+        enemyHealth = 50;
+        debugger;
+        // pass the pickedEnemyName variable's value into the fight function
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
